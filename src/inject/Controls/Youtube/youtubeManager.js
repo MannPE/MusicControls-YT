580f219bd9ecbdc204194446920aa84145120d65
Mann PE <manuelpe.icc@gmail.com>
2018-05-31 15:04:21 -0700

    Added a manager file

---
+++ src/inject/Controls/Youtube/youtubeManager.js
@@ -0,0 +1,25 @@
+
+
+function _getVideoId(youtubeUrl){
+	var res = youtubeUrl.split("v=")[1];
+	var ampersandPosition = res.indexOf('&');
+	if(ampersandPosition != -1) {
+		res = res.substring(0, ampersandPosition);
+	}
+	console.log("new Video ID:",res);
+	return res;
+}
+
+function _getVideoThumbnail(videoId){
+	var res = "https://img.youtube.com/vi/"+videoId+"/0.jpg";
+	return res;
+}
+
+function _getVideoTitle(tab){
+    return tab.title;
+}
+
+
+export const getVideoId = _getVideoId;
+export const getVideoThumbnail = _getVideoThumbnail;
+export const getVideoTitle = _getVideoTitle;
